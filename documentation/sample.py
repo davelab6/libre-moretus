e4805c874fb761b200a4f9669da4117392790048
eliheuer <eliheuer@gmail.com>
2016-12-05 23:39:35 -0500

    adding python script for generating sample.png image

---
+++ documentation/sample.py
@@ -0,0 +1,35 @@
+# from drawBot import * # uncomment if using setupAsModule.py
+import math # for cos, sin and pi functions
+
+# Print installed fonts to the terminal
+print installedFonts()
+
+# static variables
+canvasWidth = 512  # size of the image in pixels
+canvasHeight = 64
+
+# gird variables
+origin = (128, 128)
+width = 256
+height = 256
+center = width/2
+num_x_units = 8
+num_y_units = 8
+
+# draws a new page
+def new_page(): 
+    newPage(canvasWidth, canvasHeight) # new page from canvas variable 
+    fill(1) # color of background
+    rect(0, 0, canvasWidth, canvasHeight) # draw the backgroun
+new_page()
+    
+# type 
+fontSize(72)
+font("LibrePlantin-Bold")
+tracking(0)
+fill(0)
+stroke(None)
+text("Libre Plantin", (-2, 4))
+fill(1, 0, 0)
+                   
+saveImage("sample.png")
